e71204df8b4c6a68db7d8a954fc06b9e88aea2a3
ipromise2324 <168allen55@gmail.com>
2023-04-28 00:15:20 +0800

    feat:add Side Entrance(Challenge4)

--- contracts/attacker-contracts/AttackSideEntrance.sol
+++ contracts/attacker-contracts/AttackSideEntrance.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "../side-entrance/SideEntranceLenderPool.sol";
-
 contract AttackSideEntrance {
     SideEntranceLenderPool pool;
     address payable owner;
